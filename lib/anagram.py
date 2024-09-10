b63e57d1dec14e55c2a1613d81269387f33ddc3a
Sundeep <sundeep.kaler@gmail.com>
2024-09-10 13:37:26 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,23 @@
 # your code goes here!
+class Anagram:
+
+  def __init__(self, word):
+    if isinstance(word, str):
+      self.word = word
+    else:
+      print("word must be a string")
+  
+  def get_word():
+    return self._word
+  
+  def set_word(self, word):
+    self._word = word
+  
+  property(get_word, set_word)
+
+  def match(self, wordList):
+    matches = []
+    for check_word in wordList:
+      if sorted(check_word) == sorted(self.word):
+        matches.append(check_word)
+    return matches
